89e30b126e214c332b74a8200ddb721db5a21e0e
Dan <40920446+dan-k3@users.noreply.github.com>
2023-01-22 14:28:32 -0600

    Setup Django project

--- homework/settings.py
+++ homework/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nmugz%+j!sr#s#g75iggv8z8__t1v-*rg+4tt*p8s^#kh(4jib'
+SECRET_KEY = 'django-insecure-lc4!f@t+r^)%smn%qtn4i2^)xhl^42t@a#b+@$$&_p=@c=$7am'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
